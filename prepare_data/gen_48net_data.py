f659c56466a06d04d236c3e7b9268f0e039d9ac6
stoneyang <stoneyang0915@gmail.com>
2017-10-05 20:10:05 +0800

    ENH: corrected type coersion issue.

--- prepare_data/gen_48net_data.py
+++ prepare_data/gen_48net_data.py
@@ -46,7 +46,7 @@ for annotation in annotations:
 
         Iou = IoU(crop_box, boxes)
 
-        cropped_im = img[ny : ny + size, nx : nx + size, :]
+        cropped_im = img[int(ny) : int(ny + size), int(nx) : int(nx + size), :]
         resized_im = cv2.resize(cropped_im, (48, 48), interpolation=cv2.INTER_LINEAR)
 
         if np.max(Iou) < 0.3:
@@ -89,7 +89,7 @@ for annotation in annotations:
             offset_x2 = (x2 - nx2) / float(size)
             offset_y2 = (y2 - ny2) / float(size)
 
-            cropped_im = img[ny1 : ny2, nx1 : nx2, :]
+            cropped_im = img[int(ny1) : int(ny2), int(nx1) : int(nx2), :]
             resized_im = cv2.resize(cropped_im, (48, 48), interpolation=cv2.INTER_LINEAR)
 
             box_ = box.reshape(1, -1)
